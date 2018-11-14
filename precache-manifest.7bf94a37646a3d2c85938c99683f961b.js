fff0df44432ee2189c8ddf654c85d5fcedcc3296
Adam Simkins <adamsimkins@gmail.com>
2018-11-14 17:18:15 -0600

    Version 1.0

---
+++ precache-manifest.7bf94a37646a3d2c85938c99683f961b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7799912db825a5a2a124",
+    "url": "/static/js/main.7799912d.chunk.js"
+  },
+  {
+    "revision": "e45fe25c609cd9656e94",
+    "url": "/static/js/1.e45fe25c.chunk.js"
+  },
+  {
+    "revision": "7799912db825a5a2a124",
+    "url": "/static/css/main.d5195e13.chunk.css"
+  },
+  {
+    "revision": "24f7a03af7f33ae3de4aadaea090f8ee",
+    "url": "/index.html"
+  }
+];
